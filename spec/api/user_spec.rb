b8e34336aa4248e07999f6ae10c64f87e5286fdd
Andréas Livet <andreas.livet@gmail.com>
2014-03-26 22:02:13 +0100

    Add User Model Spec

--- spec/api/user_spec.rb
+++ spec/api/user_spec.rb
@@ -13,4 +13,14 @@ describe UserApi do
   	get("/user/#{u.id}").status.should == 200
   	JSON.parse(last_response.body)["id"].should == u.id
   end
+
+  it "Return 405 on wrong user" do
+  	get("/user/12356").status.should == 405
+  end
+
+  it "login a user" do
+  	credentials = {email: "test@test.com", password: "test"}
+  	u = User.create(credentials)
+  	post("/user/login", credentials).status.should == 201
+  end
 end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../helper"
+
+describe User do
+	it "Hashes passwords on creation and override ==" do
+		u = User.create(password:"test")
+		u.password.to_s.should_not == "test"
+		u.password.should == "test"
+		u.password.is_password?("test").should == true
+		u.password.is_password?("a").should == false
+	end
+end
