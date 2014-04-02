c9aab170fbf7f66065e5e279f252f08e144a4cf7
Andréas Livet <andreas.livet@gmail.com>
2014-04-02 19:31:19 +0200

    Add more spec

--- spec/api/user_spec.rb
+++ spec/api/user_spec.rb
@@ -8,10 +8,13 @@ describe UserApi do
   	UserApi
   end
 
-  it "Return a user" do
+  it "Returns a specific user" do
   	u = User.create()
   	get("/user/#{u.id}").status.should == 200
   	JSON.parse(last_response.body)["id"].should == u.id
+  end
+
+  it "Returns current logged user" do
   end
 
   it "Return 405 on wrong user" do
@@ -22,7 +25,7 @@ describe UserApi do
   	credentials = {email: "test@test.com", password: "test"}
   	u = User.create(credentials)
   	post("/user/login", credentials).status.should == 201
-  	last_response.cookies["session"].should != nil
+  	last_response.cookies["session_token"].should != nil
   end
 
   it "Create a user" do
@@ -36,9 +39,16 @@ describe UserApi do
   it "Modify a user" do
   	credentials = {email: "test@test.com", password: "test"}
   	u = User.create(credentials)
+  	#Needs to be login
+  	put("/user", {age: 19}).status.should == 401
+  	post("/user/login", credentials)
 	put("/user", {age: 19}).status.should == 201
 	User.first(email:'test@test.com').age.should == 19
 	put("/user", {age: 23}).status.should == 201
 	User.first(email:'test@test.com').age.should == 23	
+  end
+
+  it "logout a user" do
+
   end
 end
