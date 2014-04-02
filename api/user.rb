0909d5e3b3e3074caab5436c26d1e419e2d10c8b
Neocityhunters <neocityhunters@localhost.localdomain>
2014-04-02 21:05:51 +0200

    user api

--- api/user.rb
+++ api/user.rb
@@ -1,2 +1,15 @@
 class UserApi < Grape::API
+  prefix "user"
+  format :json
+  get ':id' do
+    test = params[:id]
+    message = "User Not Found"
+    user = User[test]
+    error!(message, 405) if user.nil?
+    user.values
+  end
+  post ':login' do
+    puts "Sa marche"
+    cookies[:session] = "toot"
+  end
 end
